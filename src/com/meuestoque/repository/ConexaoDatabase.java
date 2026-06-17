863b3df1719ecdc7134f389793edf02f8c359909
pedrodamassa701 <pedrocostafalcao7@gmail.com>
2026-06-16 23:39:35 -0300

    fix: puxando dados BD da env

--- src/com/meuestoque/repository/ConexaoDatabase.java
+++ src/com/meuestoque/repository/ConexaoDatabase.java
@@ -6,10 +6,9 @@ import java.sql.SQLException;
 
 public class ConexaoDatabase {
 
-    private static final String URL = "";
-    private static final String USUARIO = "postgres";
-    private static final String SENHA = "";
-
+    private static final String URL = "jdbc:postgresql://localhost:5432/estoqueDeProdutos";
+    private static final String USUARIO = System.getenv("DB_USER") != null ? System.getenv("DB_USER") : "postgres";
+    private static final String SENHA = System.getenv("DB_PASSWORD");
 
 
 }
