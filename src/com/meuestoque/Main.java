c9eacd7e8e87b1f1ad98fda86dcabb9b0159d344
pedrodamassa701 <pedrocostafalcao7@gmail.com>
2026-06-17 23:26:46 -0300

    feat: classes repository faltantes

--- src/com/meuestoque/Main.java
+++ src/com/meuestoque/Main.java
@@ -7,15 +7,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("=== Meu estoque rodando! ===");
 
-        SaborRepository saborRepo = new SaborRepository();
-
-        Sabor brownieBrigadeiro = new Sabor();
-        brownieBrigadeiro.setNomeSabor("Brigadeiro");
-        saborRepo.salvar(brownieBrigadeiro);
-
-        Sabor brownieMaracuja = new Sabor();
-        brownieMaracuja.setNomeSabor("Maracuja");
-        saborRepo.salvar(brownieMaracuja);
 
 
     }
---
+++ src/com/meuestoque/repository/LoteRepository.java
@@ -0,0 +1,4 @@
+package com.meuestoque.repository;
+
+public class LoteRepository {
+}
