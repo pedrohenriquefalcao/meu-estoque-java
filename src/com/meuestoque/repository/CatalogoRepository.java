3816fc5083135434d09b7f2e0452afcedd3d3756
pedrodamassa701 <pedrocostafalcao7@gmail.com>
2026-06-17 23:27:05 -0300

    feat: método INSERT CatalogoRepository.java

---
+++ src/com/meuestoque/repository/CatalogoRepository.java
@@ -0,0 +1,30 @@
+package com.meuestoque.repository;
+
+import com.meuestoque.model.Catalogo;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class CatalogoRepository {
+
+    public void salvar(Catalogo catalogo) {
+        String sql = "INSERT INTO catalogo (nome_produto, descricao, preco_base) VALUES (?, ?, ?)";
+
+        try (Connection conn = ConexaoDatabase.conectar();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+
+            // 🌟 CORREÇÃO 2: Preenchemos cada lacuna na ordem exata da query
+            stmt.setString(1, catalogo.getNomeProduto());
+            stmt.setString(2, catalogo.getDescricao());
+            stmt.setBigDecimal(3, catalogo.getPrecoBase());
+
+            stmt.executeUpdate();
+
+            System.out.println("Sucesso!! '" + catalogo.getNomeProduto() + "' foi adicionado ao catálogo.");
+
+        } catch (SQLException e) {
+            System.err.println("Erro ao salvar produto no catálogo!");
+            e.printStackTrace();
+        }
+    }
+}
